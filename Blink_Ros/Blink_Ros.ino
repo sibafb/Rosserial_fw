0a3e520eedb95ac09cc0c921da4ade2e50b587b1
Haruma Akimoto <siba16fb@gmail.com>
2020-12-29 16:36:47 +0900

    Blink_Led追加

---
+++ Blink_Ros/Blink_Ros.ino
@@ -0,0 +1,31 @@
+#include <ros.h>
+#include <std_msgs/Empty.h>
+
+#define LED_PIN 35
+
+ros::NodeHandle nh;
+
+void messageCb(const std_msgs::Empty& toggle_msg){
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
+}
+
+ros::Subscriver<<std_msgs::Empty> sub("toggle_led", &messageCb);
+
+void setup(){
+    pinMode(LED_PIN,OUTPUT);
+    digitalWrite(LED_PIN,HIGH);
+    nh.initNode();
+    nh/subscribe(sub);
+}
+
+void loop(){
+    nh.spinOnce();
+    delay(1);
+}
